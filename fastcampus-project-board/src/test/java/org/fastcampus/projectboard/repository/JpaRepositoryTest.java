507a5b9964c09673b045d92c2ad5616dc2b0e5ec
seodabin <seodb89@gmail.com>
2024-04-15 19:57:36 +0900

    #10 - 테스트 데이터 작성
    기본 CRUD

---
+++ fastcampus-project-board/src/test/java/org/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,109 @@
+package org.fastcampus.projectboard.repository;
+
+import org.fastcampus.projectboard.config.JpaConfig;
+import org.fastcampus.projectboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+//@ActiveProfiles("testdb")
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)   //JpaConfig 클래스는 만들어진거라 못 찾으니까 임포트 (auditing)
+@DataJpaTest
+class JpaRepositoryTest {
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    // given_when_then
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // Given
+
+
+        // When
+        List<Article> articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(123);
+
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+        // Given
+        long previousCount = articleRepository.count();
+        Article article = Article.of("new article", "new content", "#spring");
+
+        // When
+        Article savedArticle = articleRepository.save(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount+1);
+
+
+    }
+
+    /**
+     * Transaction 이 묶여 있어서 다 돌면 록백 된다-> 결국 update반영이 필요가 없어서
+     * 플러쉬로 업데이트 문은 생성 되나 역시나 반영은 X
+     */
+
+    @DisplayName("update 테스트")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updatedHashTag = "#springboot";
+
+        article.setHashtag(updatedHashTag);
+
+        // When
+        Article savedArticle = articleRepository.saveAndFlush(article);
+
+        // Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag",updatedHashTag);
+
+
+    }
+
+
+    @DisplayName("Delete 테스트")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+        // When
+        articleRepository.delete(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount-1);
+
+
+    }
+}
